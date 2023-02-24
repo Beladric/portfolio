50edd2beb56a7fa8d2a2bf45c8a3e3a30b6e7eef
Beladric <kilian.goetz@yahoo.fr>
2023-02-24 16:20:14 +0100

    Add preloader
    Add loading animation in all pages.

---
+++ scripts/loader.js
@@ -0,0 +1,6 @@
+let loader = document.getElementById("preloader");
+
+window.addEventListener("load", function(){
+    loader.style.display = "none";
+    document.body.style.overflow = "visible";
+})
