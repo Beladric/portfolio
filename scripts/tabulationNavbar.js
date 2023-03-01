2e4bcb7ed23e794df3c29f0edac8a41936c23c1f
Beladric <kilian.goetz@yahoo.fr>
2023-03-01 20:41:10 +0100

    tabNavigation style

--- scripts/tabulationNavbar.js
+++ scripts/tabulationNavbar.js
@@ -1,7 +1,10 @@
-function openTab(tabName){
+function openTab(tabName, buttonName){
     let x = document.getElementsByClassName("tab");
+    let y = document.getElementsByClassName("tabButton");
     for(let i = 0; i<x.length; i++){
         x[i].style.display = "none";
+        y[i].style.backgroundColor = "transparent";
     }
     document.getElementById(tabName).style.display = "flex";
+    document.getElementById(buttonName).style.backgroundColor = "#EEEEEE";
 }
