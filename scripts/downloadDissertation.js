01f078587922e28a264fbc32e439377a6508e5d4
Beladric <kilian.goetz@yahoo.fr>
2023-03-02 20:57:11 +0100

    JS script for masters dissertation

--- scripts/downloadDissertation.js
+++ scripts/downloadDissertation.js
@@ -1,27 +1,13 @@
-function downloadFile(url, fileName){
-    fetch(url, { method: 'get', mode: 'no-cors', referrerPolicy: 'no-referrer' })
-        .then(res => res.blob())
-        .then(res => {
-            const aElement = document.createElement('a');
-            aElement.setAttribute('download', fileName);
-            const href = URL.createObjectURL(res);
-            aElement.href = href;
-            // aElement.setAttribute('href', href);
-            aElement.setAttribute('target', '_blank');
-            aElement.click();
-            URL.revokeObjectURL(href);
-        });
-}
-function downloadDissertation(){
-    switch (document.getElementById("selectYear").value){
+const selectYear = document.getElementById("selectYear");
+selectYear.addEventListener("change", (event)=>{
+    console.log(event.target.value.toString())
+    switch (event.target.value){
         case "firstYear":
-            downloadFile('https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.edunao.com%2Fwp-content%2Fuploads%2F2022%2F07%2FMicrosoft-Azure.png&imgrefurl=https%3A%2F%2Fwww.edunao.com%2Fmoodle%2Fplugins%2Fazure%2F&tbnid=Cb8LGAcdhfiBQM&vet=12ahUKEwj6sonB3r39AhX5VaQEHSmlDrgQMygAegUIARDcAQ..i&docid=LfA0L4HCL2u2dM&w=3840&h=2171&q=azure&client=ubuntu&ved=2ahUKEwj6sonB3r39AhX5VaQEHSmlDrgQMygAegUIARDcAQ', 'test.png')
             break;
         case "secondYear":
-            downloadFile('https://plainenglish.io/blog/how-to-download-a-file-using-javascript-fec4685c0a22', 'test')
+            document.getElementById("downloadDissertationB").href = "https://www.woopets.fr/assets/img/001/244/1200x675/choisir-chien-japonais.jpg";
             break;
         case "lastYear":
-            downloadFile('https://plainenglish.io/blog/how-to-download-a-file-using-javascript-fec4685c0a22', 'test')
             break;
     }
-}
+})
