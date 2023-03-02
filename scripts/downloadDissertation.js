3526a6473577bf0dfc89309d9ee6cb6e09216975
Beladric <kilian.goetz@yahoo.fr>
2023-03-02 21:12:37 +0100

    Hotfix issue in footer
    Badly used of img height option in footer

--- scripts/downloadDissertation.js
+++ scripts/downloadDissertation.js
@@ -3,11 +3,13 @@ selectYear.addEventListener("change", (event)=>{
     console.log(event.target.value.toString())
     switch (event.target.value){
         case "firstYear":
+            document.getElementById("downloadDissertationB").href = "https://kilian-goetz.engineer/resources/Rapport%20GO%C3%8BTZ%20Kilian.pdf";
             break;
         case "secondYear":
-            document.getElementById("downloadDissertationB").href = "https://www.woopets.fr/assets/img/001/244/1200x675/choisir-chien-japonais.jpg";
+            document.getElementById("downloadDissertationB").href = "";
             break;
         case "lastYear":
+            document.getElementById("downloadDissertationB").href = "";
             break;
     }
 })
