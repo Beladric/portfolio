b7e0bd413d1de7c64c01de3110e93ffde82b7f38
Beladric <kilian.goetz@yahoo.fr>
2023-03-08 21:47:08 +0100

    CV download button script

--- scripts/downloadDissertation.js
+++ scripts/downloadDissertation.js
@@ -1,6 +1,5 @@
 const selectYear = document.getElementById("selectYear");
 selectYear.addEventListener("change", (event)=>{
-    console.log(event.target.value.toString())
     switch (event.target.value){
         case "firstYear":
             document.getElementById("downloadDissertationB").href = "https://kilian-goetz.engineer/resources/Rapport%20GO%C3%8BTZ%20Kilian.pdf";
