de11abcbf49961bbfb0cc10786dd4665c35e985b
Beladric <kilian.goetz@yahoo.fr>
2023-03-08 21:59:32 +0100

    Update CVs + CVs extension + CV download script url

--- scripts/downloadCV.js
+++ scripts/downloadCV.js
@@ -1,6 +1,6 @@
 const selectCVLanguage = document.getElementById("selectCVLanguage");
 
 selectCVLanguage.addEventListener("change",(event)=>{
-    document.getElementById("DownloadButton").href = "https://github.com/Beladric/portfolio/blob/main/resources/CV%20GO%C3%8BTZ%20Kilian%20".concat(event.target.value).concat(".png?raw=true");
+    document.getElementById("DownloadButton").href = "https://github.com/Beladric/portfolio/blob/main/resources/CV%20GO%C3%8BTZ%20Kilian%20".concat(event.target.value).concat(".pdf?raw=true");
     console.log(document.getElementById("DownloadButton").href)
 })
